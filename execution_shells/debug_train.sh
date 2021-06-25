e7bc7502c7ce5f5f72504feef2a2bce02e0823ce
SUGIYOSI <sgyslucky77@gmail.com>
2021-06-25 11:15:12 +0900

    add vit-tiny

---
+++ execution_shells/debug_train.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#$ -cwd
+#$ -l f_node=1
+#$ -l h_rt=10:00:00
+#$ -j y
+#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/execution_shells/output/o.$JOB_ID
+
+source /gs/hs0/tga-i/sugiyama.y.al/VISSL/VISSL_386/bin/activate
+module load cuda/10.2.89
+
+echo '--Start--'
+echo `date`
+
+python /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/run_distributed_engines.py \
+    hydra.verbose=true \
+    config=pretrain/vision_transformer/simclr/simclr_deit_ti16.yaml \
+    config.DATA.TRAIN.DATA_SOURCES=[disk_folder] \
+    config.DATA.TRAIN.LABEL_SOURCES=[disk_folder] \
+    config.DATA.TRAIN.DATASET_NAMES=[imagenet_1k] \
+    config.DATA.TEST.DATA_SOURCES=[disk_folder] \
+    config.DATA.TEST.LABEL_SOURCES=[disk_folder] \
+    config.DATA.TEST.DATASET_NAMES=[imagenet_1k] \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=4 \
+    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/debug_simclr_deit_imagenet1k_v2" \
+    config.HOOKS.TENSORBOARD_SETUP.USE_TENSORBOARD=true
+
+echo '--End--'
+echo `date`
+    # config=pretrain/vision_transformer/simclr/vit_t16.yaml \
+    # config=pretrain/vision_transformer/simclr/simclr_deit_ti16.yaml \
+    # config=pretrain/simclr/simclr_1node_resnet50.yaml \
