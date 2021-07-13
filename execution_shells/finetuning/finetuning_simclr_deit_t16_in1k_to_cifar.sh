e7c126d50f7460fcc79bba71ba4a91791f501ceb
SUGIYOSI <sgyslucky77@gmail.com>
2021-07-13 14:59:44 +0900

    add pretrain

---
+++ execution_shells/finetuning/finetuning_simclr_deit_t16_in1k_to_cifar.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#$ -cwd
+#$ -l f_node=1
+#$ -l h_rt=24:00:00
+#$ -j y
+#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/execution_shells/output/finetuning/o.finetuning_simclr_deit_t16_in1k_to_cifar100__v1
+
+source /gs/hs0/tga-i/sugiyama.y.al/VISSL/VISSL_386/bin/activate
+module load cuda/10.2.89
+
+echo '--Start--'
+echo `date`
+/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/execution_shells/output/finetuning/o.finetuning_simclr_deit_t16_in1k_to_cifar100__v1
+python /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/run_distributed_engines.py \
+    hydra.verbose=true \
+    config=/benchmark/fulltune/finetuning_simclr_deit.yaml \
+    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/finetuning/finetuning_simclr_deit_t16_in1k_to_cifar100__v1" \
+    config.DATA.TRAIN.DATA_SOURCES=[torchvision_dataset] \
+    config.DATA.TRAIN.LABEL_SOURCES=[torchvision_dataset] \
+    config.DATA.TRAIN.DATASET_NAMES=[CIFAR100] \
+    config.DATA.TRAIN.COPY_DESTINATION_DIR="/tmp/cifar100/" \
+    config.DATA.TEST.DATA_SOURCES=[torchvision_dataset] \
+    config.DATA.TEST.LABEL_SOURCES=[torchvision_dataset] \
+    config.DATA.TEST.COPY_DESTINATION_DIR="/tmp/cifar100/" \
+    config.DATA.TEST.DATASET_NAMES=[CIFAR100] \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=4 \
+    config.MODEL.WEIGHTS_INIT.PARAMS_FILE="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/pretrain/pretrain_simclr_deit_t16_in1k__v1/model_phase100.torch" \
+
+echo '--End--'
+echo `date`
+
+# /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/execution_shells/output/finetuning/o.finetuning_simclr_deit_t16_in1k_to_cifar10__v1
+# python /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/run_distributed_engines.py \
+#     hydra.verbose=true \
+#     config=/benchmark/fulltune/finetuning_simclr_deit.yaml \
+#     config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/finetuning/finetuning_simclr_deit_t16_in1k_to_cifar10__v1" \
+#     config.DATA.TRAIN.DATA_SOURCES=[torchvision_dataset] \
+#     config.DATA.TRAIN.LABEL_SOURCES=[torchvision_dataset] \
+#     config.DATA.TRAIN.DATASET_NAMES=[CIFAR10] \
+#     config.DATA.TRAIN.COPY_DESTINATION_DIR="/tmp/cifar10/" \
+#     config.DATA.TEST.DATA_SOURCES=[torchvision_dataset] \
+#     config.DATA.TEST.LABEL_SOURCES=[torchvision_dataset] \
+#     config.DATA.TEST.COPY_DESTINATION_DIR="/tmp/cifar10/" \
+#     config.DATA.TEST.DATASET_NAMES=[CIFAR10] \
+#     config.DISTRIBUTED.NUM_NODES=1 \
+#     config.DISTRIBUTED.NUM_PROC_PER_NODE=4 \
+#     config.MODEL.WEIGHTS_INIT.PARAMS_FILE="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/pretrain/pretrain_simclr_deit_t16_in1k__v1/model_phase100.torch" \
---
+++ execution_shells/pretrain_simclr_deit_t16_fake1k_v2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#$ -cwd
+#$ -l f_node=1
+#$ -l h_rt=116:00:00
+#$ -j y
+#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/execution_shells/output/pretrain/pretrain_simclr_deit_t16_fake1k_v2__v1_v2
+
+source /gs/hs0/tga-i/sugiyama.y.al/VISSL/VISSL_386/bin/activate
+module load cuda/10.2.89
+
+echo '--Start--'
+echo `date`
+python /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/run_distributed_engines.py \
+    hydra.verbose=true \
+    config=pretrain/vision_transformer/simclr/simclr_deit_t16.yaml \
+    config.DATA.TRAIN.DATA_SOURCES=[disk_folder] \
+    config.DATA.TRAIN.LABEL_SOURCES=[disk_folder] \
+    config.DATA.TRAIN.DATASET_NAMES=[fakeimagenet_v2_1k] \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=4 \
+    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/pretrain/pretrain_simclr_deit_t16_fake1k_v2__v1" \
+    config.HOOKS.TENSORBOARD_SETUP.USE_TENSORBOARD=true
+    config.MODEL.WEIGHTS_INIT.PARAMS_FILE="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/pretrain/pretrain_simclr_deit_t16_fake1k_v2__v1/checkpoint.torch" \
+
+echo '--End--'
+echo `date`
