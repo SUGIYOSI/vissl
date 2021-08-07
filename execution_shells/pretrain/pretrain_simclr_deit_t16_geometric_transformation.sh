0ac37c3341aec1d5aa2d91e94b1453cff0abf6a9
SUGIYOSI <sgyslucky77@gmail.com>
2021-08-08 00:51:38 +0900

    add pretrain transforms

---
+++ execution_shells/pretrain/pretrain_simclr_deit_t16_geometric_transformation.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -cwd
+#$ -l f_node=1
+#$ -l h_rt=24:00:00
+#$ -j y
+#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/execution_shells/output/pretrain/pretrain_simclr_deit_t16_geometric_transformation_in1k_10__v1__1
+
+source /gs/hs0/tga-i/sugiyama.y.al/VISSL/VISSL_386/bin/activate
+module load cuda/10.2.89
+
+echo '--Start--'
+echo `date`
+python /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/run_distributed_engines.py \
+    hydra.verbose=true \
+    config=pretrain/vision_transformer/simclr/simclr_deit_t16_geometric_transformation.yaml \
+    config.DATA.TRAIN.DATA_SOURCES=[disk_folder] \
+    config.DATA.TRAIN.LABEL_SOURCES=[disk_folder] \
+    config.DATA.TRAIN.DATASET_NAMES=[original_imagenet_1k_10] \
+    config.DISTRIBUTED.NUM_NODES=1 \
+    config.DISTRIBUTED.NUM_PROC_PER_NODE=4 \
+    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/pretrain/pretrain_simclr_deit_t16_geometric_transformation_in1k_10__v1" \
+
+echo '--End--'
+echo `date`
