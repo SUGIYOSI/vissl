3e42cf94a2f500744a19516e139e3d6ba604ff60
SUGIYOSI <sgyslucky77@gmail.com>
2021-09-14 16:39:53 +0900

    add

--- execution_shells/finetuning/finetuning_simclr_resnet50_in1k_to_cifar100.sh
+++ execution_shells/finetuning/finetuning_simclr_resnet50_in1k_to_cifar100.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 #$ -cwd
 #$ -l f_node=1
-#$ -l h_rt=03:00:00
+#$ -l h_rt=24:00:00
 #$ -j y
-#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/execution_shells/output/debug/v15/o.pretrain_simclr_deit_t16_in1k_p100_node_1__v3
+#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/execution_shells/output/finetuning/debug/o.finetuning_simclr_official_resnet50_in1k_ep200_to_cifar100__v1
 
 # ======== env ========
-source /gs/hs0/tga-i/sugiyama.y.al/VISSL/VISSL_386_cuda_102/bin/activate
-module load cuda/10.2.89 openmpi/3.1.4-opa10.10 gcc/8.3.0
+source /gs/hs0/tga-i/sugiyama.y.al/VISSL/VISSL_386/bin/activate
+module load cuda/10.2.89 openmpi/3.1.4-opa10.10
 module list
 
 # ======== configs ========
 export NPERNODE=1
 export NUM_NODES=1
 export NUM_PROC=4
-export BATCHSIZE_FOR_GPU=32
+export BATCHSIZE_FOR_GPU=64
 export BATCHSIZE=$(($BATCHSIZE_FOR_GPU*$NUM_NODES*$NUM_PROC))
 export MASTER_ADDR=$(ip addr show dev ib0 | grep '\<inet\>' | cut -d " " -f 6 | cut -d "/" -f 1)
 export RUN_ID=$MASTER_ADDR:8888
@@ -25,17 +25,16 @@ echo `date`
 mpirun -npernode $NPERNODE -np $NUM_NODES \
 python /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/run_distributed_engines.py \
     hydra.verbose=true \
-    config=/pretrain/simclr/simclr_resnet50.yaml \
-    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/other/v15/pretrain_simclr_deit_t16_in1k_p100_node_1__v3" \
-    config.DATA.TRAIN.DATASET_NAMES=[original_imagenet_1k] \
+    config=benchmark/fulltune/cifar100/finetuning_simclr_resnet_to_cifar100.yaml \
+    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/debug/finetuning/finetuning_simclr_official_resnet50_in1k_ep200_to_cifar100__v1" \
+    config.MODEL.WEIGHTS_INIT.PARAMS_FILE="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/distribution/simclr/model_final_checkpoint_phase199.torch" \
     config.DISTRIBUTED.NUM_NODES=$NUM_NODES \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=$NUM_PROC \
     config.MODEL.SYNC_BN_CONFIG.GROUP_SIZE=$NUM_PROC \
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=$BATCHSIZE_FOR_GPU \
+    config.DATA.TEST.BATCHSIZE_PER_REPLICA=$BATCHSIZE_FOR_GPU \
     config.OPTIMIZER.param_schedulers.lr.auto_lr_scaling.base_lr_batch_size=$BATCHSIZE \
     config.DISTRIBUTED.RUN_ID=$RUN_ID \
 
 echo '--End--'
 echo `date`
-    # config.DISTRIBUTED.RUN_ID=$MASTER_ADDR$PORT \
-    # config.DISTRIBUTED.INIT_METHOD="file" \
