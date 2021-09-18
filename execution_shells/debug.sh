3f1f49213281cc73dfc001178ffc57688a88b36b
SUGIYOSI <sgyslucky77@gmail.com>
2021-09-18 17:17:22 +0900

    add

--- execution_shells/debug.sh
+++ execution_shells/debug.sh
@@ -1,28 +1,15 @@
 #!/bin/bash
 #$ -cwd
 #$ -l f_node=4
-#$ -l h_rt=00:10:00
+#$ -l h_rt=02:00:00
 #$ -j y
-#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/execution_shells/output/debug/v2/o.pretrain_simclr_deit_in1k_p100_node_4__v18
-
-# ======== env ========
-# . /etc/profile.d/modules.sh
-# module load cuda/11.0.194 gcc/8.3.0 cudnn/8.1 nccl/2.8.4 openmpi/3.1.4-opa10.10
-# export PATH="/gs/hs0/tga-i/sugiyama.y.al/anaconda3/bin:${PATH}"
-# source VISSL_py371_cu110_pyt171
-# module list
-
-# ======== env ========
-# . /etc/profile.d/modules.sh
-# module load cuda/10.2.89 cudnn/7.6 openmpi/3.1.4-opa10.10 gcc/8.3.0
-# export PATH="/gs/hs0/tga-i/sugiyama.y.al/anaconda3/bin:${PATH}"
-# source activate VISSL_py371_cu102_pyt171
-# module list
+#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/output__tsubame/debug/v2/o.pretrain_simclr_deit_in1k_p100_node_4__v2
 
 # ======== env ========
 . /etc/profile.d/modules.sh
-module load cuda/10.2.89 cudnn/7.6 openmpi/3.1.4-opa10.10 gcc/8.3.0
-source /gs/hs0/tga-i/sugiyama.y.al/VISSL/VISSL_386/bin/activate
+module load cuda/11.0.194 gcc/8.3.0 cudnn/8.1 nccl/2.8.4 openmpi/3.1.4-opa10.10
+export PATH="/gs/hs0/tga-i/sugiyama.y.al/anaconda3/bin:${PATH}"
+source activate VISSL_py371_cu110_pyt171
 module list
 
 # ======== configs ========
@@ -33,19 +20,16 @@ export BATCHSIZE_FOR_GPU=128
 export BATCHSIZE=$(($BATCHSIZE_FOR_GPU*$NUM_NODES*$NUM_PROC))
 export MASTER_ADDR=$(ip addr show dev ib0 | grep '\<inet\>' | cut -d " " -f 6 | cut -d "/" -f 1)
 export RUN_ID=$MASTER_ADDR:8888
-
-cd /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl
-HOST=${HOSTNAME:0:5}
-# cat $HOST > ./hostfile
+export NCCL_IB_DISABLE=1
 
 # ======== script ========
 echo '--Start--'
 echo `date`
 mpirun -npernode $NPERNODE -np $NUM_NODES \
-python -B /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/run_distributed_engines.py \
+python -B /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/vissl/run_distributed_engines.py \
     hydra.verbose=true \
     config=/pretrain/vision_transformer/simclr/simclr_deit_t16.yaml \
-    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/debug/other/v2/pretrain_simclr_deit_in1k_p100_node_4__v18" \
+    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result__tsubame/debug/v2/pretrain_simclr_deit_in1k_p100_node_4__v2" \
     config.DATA.TRAIN.DATASET_NAMES=[original_imagenet_1k] \
     config.DISTRIBUTED.NUM_NODES=$NUM_NODES \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=$NUM_PROC \
@@ -53,10 +37,9 @@ python -B /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/run_distributed_engines.py \
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=$BATCHSIZE_FOR_GPU \
     config.OPTIMIZER.param_schedulers.lr.auto_lr_scaling.base_lr_batch_size=$BATCHSIZE \
     config.DISTRIBUTED.RUN_ID=$RUN_ID \
-    # config.DISTRIBUTED.HOST=${HOST} \
 
 echo '--End--'
 echo `date`
 
-# config=/pretrain/vision_transformer/simclr/simclr_deit_t16.yaml \
-# config.DISTRIBUTED.RUN_ID=$RUN_ID \
+# ======== configs ========
+# config=pretrain/vision_transformer/simclr/simclr_deit_t16.yaml \
--- execution_shells/test/test_beeond.sh
+++ execution_shells/test/test_beeond.sh
@@ -1,39 +1,46 @@
 #!/bin/bash
 #$ -cwd
-#$ -l f_node=1
-#$ -l h_rt=01:00:00
+#$ -l f_node=2
+#$ -l h_rt=00:02:00
 #$ -j y
-#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/execution_shells/output/debug/v1/o.pretrain_simclr_deit_t16_in1k_p100_node_1_beeyond__v1
+#$ -v USE_BEEOND=1
+#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/output__tsubame/debug/v3/o.debug_beeyond
 
 # ======== env ========
-source /gs/hs0/tga-i/sugiyama.y.al/VISSL/VISSL_386_cuda_102/bin/activate
-module load cuda/10.2.89 cudnn/7.6 openmpi/3.1.4-opa10.10 gcc/8.3.0-cuda
+. /etc/profile.d/modules.sh
+module load cuda/11.0.194 gcc/8.3.0 cudnn/8.1 nccl/2.8.4 openmpi/3.1.4-opa10.10
+export PATH="/gs/hs0/tga-i/sugiyama.y.al/anaconda3/bin:${PATH}"
+source activate VISSL_py371_cu110_pyt171
 module list
 
 # ======== configs ========
 export NPERNODE=1
-export NUM_NODES=1
+export NUM_NODES=4
 export NUM_PROC=4
-export BATCHSIZE_FOR_GPU=64
+export BATCHSIZE_FOR_GPU=128
 export BATCHSIZE=$(($BATCHSIZE_FOR_GPU*$NUM_NODES*$NUM_PROC))
 export MASTER_ADDR=$(ip addr show dev ib0 | grep '\<inet\>' | cut -d " " -f 6 | cut -d "/" -f 1)
 export RUN_ID=$MASTER_ADDR:8888
+export NCCL_IB_DISABLE=1
 
 # ======== script ========
-echo '--Start--'
-echo `date`
-mpirun -npernode $NPERNODE -np $NUM_NODES \
-python /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/run_distributed_engines.py \
-    hydra.verbose=true \
-    config=/debug/simclr_deit_ti16.yaml \
-    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result/other/v1/pretrain_simclr_deit_t16_in1k_p100_node_1_beeyond__v1" \
-    config.DATA.TRAIN.DATASET_NAMES=[original_imagenet_1k] \
-    config.DISTRIBUTED.NUM_NODES=$NUM_NODES \
-    config.DISTRIBUTED.NUM_PROC_PER_NODE=$NUM_PROC \
-    config.MODEL.SYNC_BN_CONFIG.GROUP_SIZE=$NUM_PROC \
-    config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=$BATCHSIZE_FOR_GPU \
-    config.OPTIMIZER.param_schedulers.lr.auto_lr_scaling.base_lr_batch_size=$BATCHSIZE \
-    config.DISTRIBUTED.RUN_ID=$RUN_ID \
+# echo '--Start--'
+# echo `date`
+# mpirun -npernode $NPERNODE -np $NUM_NODES \
+# python -B /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/vissl/run_distributed_engines.py \
+#     hydra.verbose=true \
+#     config=/pretrain/vision_transformer/simclr/simclr_deit_t16.yaml \
+#     config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result__tsubame/debug/v2/pretrain_simclr_deit_in1k_p100_node_4__v2" \
+#     config.DATA.TRAIN.DATASET_NAMES=[original_imagenet_1k] \
+#     config.DISTRIBUTED.NUM_NODES=$NUM_NODES \
+#     config.DISTRIBUTED.NUM_PROC_PER_NODE=$NUM_PROC \
+#     config.MODEL.SYNC_BN_CONFIG.GROUP_SIZE=$NUM_PROC \
+#     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=$BATCHSIZE_FOR_GPU \
+#     config.OPTIMIZER.param_schedulers.lr.auto_lr_scaling.base_lr_batch_size=$BATCHSIZE \
+#     config.DISTRIBUTED.RUN_ID=$RUN_ID \
 
-echo '--End--'
-echo `date`
+# echo '--End--'
+# echo `date`
+
+# ======== configs ========
+# config=pretrain/vision_transformer/simclr/simclr_deit_t16.yaml \
