e74805d2ecfec61899b89062e3ea55d32c4f2966
SUGIYOSI <sgyslucky77@gmail.com>
2021-09-20 16:04:33 +0900

    add config

--- execution_shells/pretrain/simclr/pretrain_simclr_deit_t16.sh
+++ execution_shells/pretrain/simclr/pretrain_simclr_deit_t16.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #$ -cwd
-#$ -l f_node=2
+#$ -l f_node=8
 #$ -l h_rt=24:00:00
 #$ -j y
-#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/output__tsubame/debug/v4/o.test_finetuning_resnet50_to_cifar10__v1
+#$ -v USE_BEEOND=1
+#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/output__tsubame/pretrain/simclr/pretrain_simclr_deit_t16_in1k_per10__v1
 
 # ======== env ========
 . /etc/profile.d/modules.sh
@@ -14,9 +15,9 @@ module list
 
 # ======== configs ========
 export NPERNODE=1
-export NUM_NODES=2
+export NUM_NODES=8
 export NUM_PROC=4
-export BATCHSIZE_FOR_GPU=32
+export BATCHSIZE_FOR_GPU=64
 export BATCHSIZE=$(($BATCHSIZE_FOR_GPU*$NUM_NODES*$NUM_PROC))
 export MASTER_ADDR=$(ip addr show dev ib0 | grep '\<inet\>' | cut -d " " -f 6 | cut -d "/" -f 1)
 export RUN_ID=$MASTER_ADDR:8888
@@ -24,11 +25,11 @@ export NCCL_IB_DISABLE=1
 export BEEONDDIR="/beeond"
 
 # ======== use beeond ========
-# echo '--Com Start--'
-# echo `date`
-# tar -xf /gs/hs0/tga-i/sugiyama.y.al/datasets/ILSVRC2012/originalimages.tar.gz --use-compress-program=pigz -C $BEEONDDIR
-# echo '--Com End--'
-# echo `date`
+echo '--Com Start--'
+echo `date`
+tar -xf /gs/hs0/tga-i/sugiyama.y.al/datasets/ILSVRC2012/originalimages_per10.tar.gz --use-compress-program=pigz -C $BEEONDDIR
+echo '--Com End--'
+echo `date`
 
 # ======== script ========
 echo '--Start--'
@@ -36,14 +37,13 @@ echo `date`
 mpirun -npernode $NPERNODE -np $NUM_NODES \
 python -B /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/vissl/run_distributed_engines.py \
     hydra.verbose=true \
-    config=/benchmark/fulltune/cifar10/finetuning_simclr_resnet_to_cifar10.yaml \
-    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result__tsubame/debug/v4/test_finetuning_resnet50_to_cifar10__v1" \
-    config.MODEL.WEIGHTS_INIT.PARAMS_FILE="" \
+    config=/pretrain/vision_transformer/simclr/simclr_deit_t16.yaml \
+    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result__tsubame/pretrain/simclr/pretrain_simclr_deit_t16_in1k_per10__v1" \
+    config.DATA.TRAIN.DATASET_NAMES=[beeond_imagenet1k-per10] \
     config.DISTRIBUTED.NUM_NODES=$NUM_NODES \
     config.DISTRIBUTED.NUM_PROC_PER_NODE=$NUM_PROC \
     config.MODEL.SYNC_BN_CONFIG.GROUP_SIZE=$NUM_PROC \
     config.DATA.TRAIN.BATCHSIZE_PER_REPLICA=$BATCHSIZE_FOR_GPU \
-    config.DATA.TEST.BATCHSIZE_PER_REPLICA=$BATCHSIZE_FOR_GPU \
     config.OPTIMIZER.param_schedulers.lr.auto_lr_scaling.base_lr_batch_size=$BATCHSIZE \
     config.DISTRIBUTED.RUN_ID=$RUN_ID \
 
