54ac46fa7f326ac58aa36efaa9ba5efafe29d379
SUGIYOSI <sgyslucky77@gmail.com>
2021-10-12 19:21:09 +0900

    add config experiment01

---
+++ execution_shells/experiment_01/finetuning/finetuning_resnet50_gaussian_blur_in1k_to_cifar10.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -cwd
+#$ -l f_node=1
+#$ -l h_rt=16:00:00
+#$ -j y
+#$ -o /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/output__tsubame/experiment_01/finetuning/o.finetuning_simclr_resnet50_gaussian_blur_oneside_in1k_per10_to_cifar10__v1
+
+# ======== env ========
+. /etc/profile.d/modules.sh
+module load cuda/10.2.89 cudnn/7.6 openmpi/3.1.4-opa10.10 gcc/8.3.0
+export PATH="/gs/hs0/tga-i/sugiyama.y.al/anaconda3/bin:${PATH}"
+source activate VISSL_py371_cu102_pyt171
+module list
+
+# ======== script ========
+echo '--Start--'
+echo `date`
+python /gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/vissl/run_distributed_engines.py \
+    config=/benchmark/fulltune/cifar10/finetuning_resnet50_to_cifar10.yaml \
+    config.CHECKPOINT.DIR="/gs/hs0/tga-i/sugiyama.y.al/VISSL/vissl/train_result__tsubame/experiment_01/finetuning/finetuning_simclr_resnet50_gaussian_blur_oneside_in1k_per10_to_cifar10__v1" \
+    config.MODEL.WEIGHTS_INIT.PARAMS_FILE="" \
+
+echo '--End--'
+echo `date`
